0442a14d47b92e134a62b4210f3e1bc3b66271b5
Damian <damiankaminski22@gmail.com>
2018-12-04 18:40:02 +0100

    notes added

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,30 +1,85 @@
 <template>
-<!-- scaffold   -->
-<section><h1>Dashboard</h1>
-<h1 v-if="!user">Getting user information...</h1>
-<h1 v-if="user">Hello {{user.username}}</h1>
+  <!-- scaffold   -->
+  <section>
+    <h1>Dashboard</h1>
+    <h1 v-if="!user">Getting user information...</h1>
+    <h1 v-if="user">Hello {{user.username}} 🙌</h1>
+    <button @click="logout()" class="btn btn-primary">Log Out</button>
+    <br>
+    <br>
+    <button @click="showForm = !showForm" class="btn btn-info">Toggle form</button>
+    <br>
+    <br>
+    <form v-if="showForm" @submit.prevent="addNote()">
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input
+          v-model="newNote.title"
+          type="text"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="titleHelp"
+          placeholder="Enter a title"
+          required
+        >
+        <small id="titleHelp" class="form-text text-muted">Enter a descriptive title for your note.</small>
+      </div>
 
-<button @click="logout()" class="btn btn-primary">Log Out</button>
-</section>
-
+      <div class="form-group">
+        <label for="note">Note</label>
+        <textarea
+          v-model="newNote.note"
+          class="form-control"
+          id="note"
+          rows="3"
+          placeholder="Enter your note"
+          required
+        ></textarea>
+      </div>
+      <button type="submit" class="btn btn-success">Add note</button>
+    </form>
+    <section class="row mt-3">
+      <div class="col-6" v-for="note in notes" :key="note._id">
+        <div class="card border-primary mb-3">
+          <div class="card-header">{{note.title}}</div>
+          <div class="card-body">
+            <p class="card-text" v-html="renderMarkDown(note.note)"></p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </section>
 </template>
 
 <script>
-const API_URL = 'http://localhost:5000/';
+import MarkownIt from "markdown-it";
+import MDemoji from "markdown-it-emoji";
+const md = new MarkownIt();
+md.use(MDemoji);
+
+const API_URL = "http://localhost:5000/";
 export default {
   data: () => ({
-    user: {},
+    showForm: false,
+    user: null,
+    newNote: {
+      title: "",
+      note: ""
+    },
+    notes: []
   }),
   mounted() {
+    //MAKE SURE THAT LOGGED IN
     fetch(API_URL, {
       headers: {
-        authorization: `Bearer ${localStorage.token}`,
-      },
+        authorization: `Bearer ${localStorage.token}`
+      }
     })
       .then(res => res.json())
       .then(result => {
         if (result.user) {
           this.user = result.user;
+          this.getNotes();
         } else {
           this.logout();
         }
@@ -32,13 +87,51 @@ export default {
   },
 
   methods: {
-    logout() {
-      localStorage.removeItem('token');
-      this.$router.push('/login');
+    renderMarkDown(note) {
+      return md.render(note);
     },
-  },
+    getNotes() {
+      fetch(`${API_URL}api/v1/notes`, {
+        headers: {
+          authorization: `Bearer ${localStorage.token}`
+        }
+      })
+        .then(res => res.json())
+        .then(notes => {
+          this.notes = notes;
+        });
+    },
+    addNote() {
+      fetch(`${API_URL}api/v1/notes`, {
+        method: "post",
+        body: JSON.stringify(this.newNote),
+        headers: {
+          "content-type": "application/json",
+          authorization: `Bearer ${localStorage.token}`
+        }
+      })
+        .then(res => res.json())
+        .then(note => {
+          console.log(note);
+          //pushing note into array
+          this.notes.push(note);
+          this.newNote = {
+            title: "",
+            note: ""
+          };
+          this.showForm = false;
+        });
+    },
+    logout() {
+      localStorage.removeItem("token");
+      this.$router.push("/login");
+    }
+  }
 };
 </script>
 
 <style>
+.card {
+  margin: 0 5px;
+}
 </style>
--- server/api/notes.js
+++ server/api/notes.js
@@ -1,9 +1,50 @@
 const express = require('express');
+const Joi = require('joi');
+const db = require('../db/connection');
+const notes = db.get('notes');
+
+const schema = Joi.object().keys({
+  title: Joi.string()
+    .trim()
+    .max(100)
+    .required(),
+  note: Joi.string()
+    .trim()
+    .min(10)
+    .required(),
+});
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json([]);
+  notes
+    .find({
+      user_id: req.user._id,
+    })
+    .then(notes => {
+      res.json(notes);
+    });
+});
+
+//NOTE VALIDATION
+router.post('/', (req, res, next) => {
+  const result = Joi.validate(req.body, schema);
+  if (result.error === null) {
+    //add id to note for specify individual note
+    const note = {
+      ...req.body,
+      user_id: req.user._id,
+    };
+    //insert into db
+    notes.insert(note).then(note => {
+      res.json(note);
+    });
+  } else {
+    // ERROR
+    const error = new Error(result.error);
+    res.status(422);
+    next(error);
+  }
 });
 
 module.exports = router;
